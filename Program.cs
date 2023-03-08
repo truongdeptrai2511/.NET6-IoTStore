a4ecf0b472c911def6e89612e63d5d6ea5e50a11
NhanDao <dtnhan.ete.dut@gmail.com>
2023-03-08 21:32:21 +0700

    'add-connection-string'

--- Program.cs
+++ Program.cs
@@ -1,13 +1,28 @@
+using IotSupplyStore.DataAccess;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+// "DevelopEnvironment" is using for database of developer, change it to "DefaultConnection" if want to use database server
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(
+    builder.Configuration.GetConnectionString("DevelopEnvironment")));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+//services cors
+builder.Services.AddCors(p => p.AddPolicy("AllowAllHeadersPolicy", builder =>
+{
+    builder.WithOrigins("*").AllowAnyMethod().AllowAnyHeader();
+}));
+
 var app = builder.Build();
+
+app.UseCors("AllowAllHeadersPolicy");
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
