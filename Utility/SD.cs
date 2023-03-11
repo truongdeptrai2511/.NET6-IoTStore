d8a1adf6cf5f3f057e6910c59baaf6ef3e376a6a
Dao Trong Nhan <95452303+Rekiichan@users.noreply.github.com>
2023-03-11 17:42:31 +0700

    Add Role

---
+++ Utility/SD.cs
@@ -0,0 +1,9 @@
+﻿namespace IotSupplyStore.Utility
+{
+    public class SD
+    {
+        public const string Role_Admin = "admin";
+        public const string Role_Employee = "employee";
+        public const string Role_Customer = "customer";
+    }
+}
