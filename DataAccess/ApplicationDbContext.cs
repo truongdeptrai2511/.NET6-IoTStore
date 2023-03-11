bc1990e243b247e7805c3b0f5adc9c39260116ef
Dao Trong Nhan <95452303+Rekiichan@users.noreply.github.com>
2023-03-11 17:41:07 +0700

    Add identity for dbcontext

--- DataAccess/ApplicationDbContext.cs
+++ DataAccess/ApplicationDbContext.cs
@@ -1,22 +1,24 @@
 ﻿using IotSupplyStore.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace IotSupplyStore.DataAccess
 {
-    public class ApplicationDbContext : DbContext
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
-        public DbSet<AdminUser> AdminUsers { get; set; }
         public DbSet<Product> Products { get; set; }
         public DbSet<Category> Categories { get; set; }
-        public DbSet<Customer> Customers { get; set; }
         public DbSet<DetailProduct> DetailsProducts { get; set; }
         public DbSet<Images> Images { get; set; }
         public DbSet<Order> Orders { get; set; }
         public DbSet<Suppliers> Suppliers { get; set; }
         public DbSet<Transactions> Transactions { get; set; }
+        public DbSet<ApplicationUser> User { get; set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            base.OnModelCreating(modelBuilder);
+
             modelBuilder.Entity<Product>()
                         .HasMany(p => p.Orders)
                         .WithMany(c => c.Products)
