fc1f375453552b6852fc4e5cb22f20126039486a
Dao Trong Nhan <95452303+Rekiichan@users.noreply.github.com>
2023-03-11 17:41:47 +0700

    add login and register models

---
+++ Models/DtoModel/LoginRequestDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace IotSupplyStore.Models.DtoModel
+{
+    public class LoginRequestDTO
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
