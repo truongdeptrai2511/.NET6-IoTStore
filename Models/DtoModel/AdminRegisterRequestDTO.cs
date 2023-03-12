b04b274368ffde0a086d7ef499bb4d2db96a365a
Dao Trong Nhan <95452303+Rekiichan@users.noreply.github.com>
2023-03-13 00:26:44 +0700

    Add Field Username

--- Models/DtoModel/AdminRegisterRequestDTO.cs
+++ Models/DtoModel/AdminRegisterRequestDTO.cs
@@ -2,7 +2,8 @@
 {
     public class AdminRegisterRequestDTO
     {
-        public string Name { get; set; }
+        public string FullName { get; set; }
+        public string UserName { get; set; }
         public string Password { get; set; }
     }
 }
--- Models/DtoModel/CustomerRegisterRequestDTO.cs
+++ Models/DtoModel/CustomerRegisterRequestDTO.cs
@@ -2,6 +2,7 @@
 {
     public class CustomerRegisterRequestDTO
     {
+        public string UserName { get; set; }
         public string Name { get; set; }
         public string Password { get; set; }
         public string Email { get; set; }
