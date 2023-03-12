2d10a4496a428ec06a4a572c81a92a68d35cd55a
Dao Trong Nhan <95452303+Rekiichan@users.noreply.github.com>
2023-03-13 00:27:10 +0700

    add field time request employee

---
+++ Migrations/20230312172057_addTimeRequestTableEmployee.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IotSupplyStore.Migrations
+{
+    /// <inheritdoc />
+    public partial class addTimeRequestTableEmployee : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "RequestAt",
+                table: "EmployeeRequests",
+                type: "datetime2(0)",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RequestAt",
+                table: "EmployeeRequests");
+
+        }
+    }
+}
