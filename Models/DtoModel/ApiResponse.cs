a45d790416994262925d9e96844353c752b0cc02
Dao Trong Nhan <95452303+Rekiichan@users.noreply.github.com>
2023-03-26 00:08:55 +0700

    Make response

---
+++ Models/DtoModel/ApiResponse.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json.Serialization;
+using System.Net;
+
+namespace IotSupplyStore.Models.DtoModel
+{
+    public class ApiResponse
+    {
+        public ApiResponse()
+        {
+            ErrorMessages = new List<string>();
+        }
+
+        public HttpStatusCode StatusCode { get; set; }
+        public string Message { get; set; }
+        public object Result { get; set; } = null;
+        public List<string> ErrorMessages { get; set; }
+    }
+}
