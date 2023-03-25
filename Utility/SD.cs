021566855efa1a21a9e9908fda0e7b8016c295a7
Dao Trong Nhan <95452303+Rekiichan@users.noreply.github.com>
2023-03-26 00:10:02 +0700

    Add Status Const

--- Utility/SD.cs
+++ Utility/SD.cs
@@ -5,5 +5,17 @@
         public const string Role_Admin = "admin";
         public const string Role_Employee = "employee";
         public const string Role_Customer = "customer";
+
+        public const string StatusPending = "Pending";
+        public const string StatusApproved = "Approved";
+        public const string StatusInProcess = "InProcess";
+        public const string StatusShipped = "Shipped";
+        public const string StatusCancelled = "Cancelled";
+        public const string StatusRefunded = "Refunded";
+
+        public const string PaymentStatusPending = "Pending";
+        public const string PaymentStatusApproved = "Approved";
+        public const string PaymentStatusDelayedPayment = "DelayedPayment";
+        public const string PaymentStatusRejected = "Rejected";
     }
 }
