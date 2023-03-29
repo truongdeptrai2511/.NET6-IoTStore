85a56dbd6168c9524fec07418a5392e7f4aeb57b
Dao Trong Nhan <95452303+Rekiichan@users.noreply.github.com>
2023-03-29 13:10:22 +0700

    Add UserName field

--- Models/ViewModel/UserVM.cs
+++ Models/ViewModel/UserVM.cs
@@ -3,8 +3,9 @@
     public class UserVM
     {
         public string Id { get; set; }
-        public List<string> Role { get; set; }
+        public string UserName { get; set; }
         public string FullName { get; set; }
+        public List<string> Role { get; set; }
         public string Phone { get; set; }
         public string Email { get; set; }
         public string Avatar { get; set; }
