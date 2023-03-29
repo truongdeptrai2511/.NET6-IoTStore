4c0c623c6ee50b289da68034bfe4296b642e8544
Dao Trong Nhan <95452303+Rekiichan@users.noreply.github.com>
2023-03-29 08:32:00 +0700

    Add policy utility

--- Utility/SD.cs
+++ Utility/SD.cs
@@ -7,18 +7,25 @@
         public const string Role_Shipper = "shipper";
         public const string Role_Customer = "customer";
 
+        public const string Policy_SuperAdmin = "Super Admin";
+        public const string Policy_AccountManager = "Account Manager";
+        public const string Policy_CategoryManager = "Category Manager";
+        public const string Policy_ProductManager = "Product Manager";
+        public const string Policy_SupplierManager = "SupplierManager";
+        public const string Policy_OrderProcess = "Order Process";
+        public const string Policy_EditProfile = "Edit Profile";
+        public const string Policy_MakeOrder = "Make Order";
 
+        public const string Status_Pending = "Pending";
+        public const string Status_Approved = "Approved";
+        public const string Status_InProcess = "InProcess";
+        public const string Status_Shipped = "Shipped";
+        public const string Status_Cancelled = "Cancelled";
+        public const string Status_Refunded = "Refunded";
 
-        public const string StatusPending = "Pending";
-        public const string StatusApproved = "Approved";
-        public const string StatusInProcess = "InProcess";
-        public const string StatusShipped = "Shipped";
-        public const string StatusCancelled = "Cancelled";
-        public const string StatusRefunded = "Refunded";
-
-        public const string PaymentStatusPending = "Pending";
-        public const string PaymentStatusApproved = "Approved";
-        public const string PaymentStatusDelayedPayment = "DelayedPayment";
-        public const string PaymentStatusRejected = "Rejected";
+        public const string Payment_StatusPending = "Pending";
+        public const string Payment_StatusApproved = "Approved";
+        public const string Payment_StatusDelayedPayment = "DelayedPayment";
+        public const string Payment_StatusRejected = "Rejected";
     }
 }
