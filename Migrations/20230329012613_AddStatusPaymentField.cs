1c584f48a5feab7c5787b17662848b804c951bfd
Dao Trong Nhan <95452303+Rekiichan@users.noreply.github.com>
2023-03-29 08:31:11 +0700

    add Db Init

---
+++ Migrations/20230329012613_AddStatusPaymentField.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IotSupplyStore.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddStatusPaymentField : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "StatusPayment",
+                table: "OrderStatus",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "StatusPayment",
+                table: "OrderStatus");
+        }
+    }
+}
